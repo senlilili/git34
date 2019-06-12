2abaf894c3ba56dbdc72403af30b87c5b8731c37
senlilili <305961740@qq.com>
2019-06-12 17:03:20 +0800

    第一次上传

---
+++ Test/src/com/test/s/Test.java
@@ -0,0 +1,15 @@
+package com.test.s;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner in=new Scanner(System.in);
+  System.out.println("请输入一个数");
+  int i=in.nextInt();
+  System.out.println(i);
+	}
+
+}
